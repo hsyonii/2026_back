97e0e708a1e78f23f075d4eef0cce8ab369a7ddf
hsyonii <thdus2831@gmail.com>
2026-07-28 11:12:08 +0900

    first commit

---
+++ src/day0728/Exam1.java
@@ -0,0 +1,13 @@
+package day0728; // package(폴더명): 클래스의 위치 식별용도
+
+public class Exam1 { 
+    // 클래스 안, main 밖 에서 선언/만들기 만 가능하다.
+    int a = 10;
+
+    // *메인 함수(메인 스레드): 번역한 코드를 읽어주는 흐름단위
+    // m + enter: main() 자동완성
+    public static void main(String[] args){
+        //so + enter
+        System.out.println("안녕"); // = console.log, ;으로 문장 구분/컴파일
+    }
+}
