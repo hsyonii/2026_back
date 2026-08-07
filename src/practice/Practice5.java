cbe12a9cd1b1d5582a392d235c95c084cf8c6a75
hsyonii <thdus2831@gmail.com>
2026-08-08 00:04:16 +0900

    practice5

---
+++ src/practice/Practice5.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class Practice5 {
+    public static void main(String[] args) {
+        //문제1
+        for(int i=1; i<=10; i++)
+            System.out.println(i);
+
+        //문제2
+        for(int i=10; i>=1; i--)
+            System.out.println(i);
+
+        //문제3
+        int 합계=0;
+        for(int i=1; i<=50; i++){
+            합계 = 합계+i;
+        }
+        System.out.println(합계);
+
+        //문제4
+        for(int i=1; i<=20; i++){
+            if(i%2==0)
+                System.out.println(i);
+        }
+
+        //문제5
+        for(int i =1; i<=30; i++){
+            if(i)
+        }
+    }
+}
