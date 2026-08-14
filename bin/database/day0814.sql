a66f87cbf9131e474f2084fd8f91423d8c1a855e
hsyonii <thdus2831@gmail.com>
2026-08-14 17:04:05 +0900

    bookController

---
+++ bin/database/day0814.sql
@@ -0,0 +1,59 @@
+# 1. 데이터베이스 생성 
+drop database if exists mydb0814; -- 만약에 'mydb0814' 데이터베이스 존재하면 삭제
+create database mydb0814;   -- 'mydb0814' 데이터베이스 생성 
+use mydb0814;      -- 'mydb0814' 데이터베이스 사용 , 워크벤치 재실행후에도
+# 2. 테이블 생성 
+create table table1(     -- 'table1' 테이블 생성 
+ num_pk int ,      -- 'num_pk' 속성/컬럼/필드 을 int 타입 선언 
+    constraint primary key( num_pk ) -- 선언된 'num_pk' 속성을 pk(식별키) 설정 
+);
+# 2. 테이블 생성 
+create table table2(
+ no_pk int , 
+    constraint primary key( no_pk ) ,
+    num_fk int ,        -- 'num_fk' 속성 을 int 타입 선언  
+    constraint foreign key( num_fk ) references table1( num_pk )   -- 선언된 'num_fk'' 속성을 fk(참조키) 설정 'table1' 테이블의 'num_pk' 를 참조 
+    on update cascade  on delete cascade  -- 만약에 table1 의 'num_pk' 값이 수정/삭제 되면 'num_fk' 값도 같이 수정/삭제 
+); 
+# 3. 서로 다른 테이블 합치기 = JOIN , # PK레코드와 FK레코드를 하나의 테이블로 조회  
+# 샘플 데이터 
+insert into table1 values( 1 ) , (2) , (3) , (4) , (5);
+insert into table2 values( 1 , 1 ) , ( 2 , 2 ) , ( 3 , 1 ) , (4 , 1 ) , (5, 2);
+# 레코드 조회 
+select * from table1;
+select * from table2;
+
+# 1. 교집합
+SELECT * From table1 , table2;
+
+-- where
+SELECT* FROM table1, table2 where table1.num_pk = table2.num_fk;
+SELECT * From table1 t1, table2 t2 WHERE t1.num_pk = t2.num_fk;
+
+-- **테이블A inner join 테이블B on 조인조건
+select * from table t1 Inner Join table2 t2 ON t1.num_pk = t2.num_fk;
+
+-- 테이블A join 테이블B on 조인조건
+SELECT * from  table1 t1 JOIN table2 t2 ON t1.num_pk = t2.num_fk;
+
+-- 테이블A NATURAL JOIN 테이블B *전제조건 : PK와 FK 필드명 같은 경우
+SELECT * FROM table1 t1 NATURAL JOIN table2 t2;
+
+-- 테이블A JOIN 테이블B USING (필드명) *전제조건: pK와 FK 필드명 같은경우
+SELECT * FROM table1 t1 join table2 t2 USING (num);
+
+# OUTER JOIN
+-- 테이블A LEFT OUTER JOIN 테이블B ON, 왼쪽 테이블에 모든 레코드와 오른쪽 테이블의 교집합되는 조회
+SELECT * FROM table1 t1 LEFT OUTER JOIN table2 t2 ON t1.num_pk = t2.num_fk;
+-- 테이블A RIGHT OUTER JOIN 테이블B ON, 오른쪽 테이블에 모든레코드와 왼쪽 테이블에 교집합 되는 조회
+SELECT * FROM table1 t1 RIGHT OUTER JOIN table2 t2 ON t1.num_pk = t2.num_fk;
+#OUTER 생략가능
+SELECT * FROM table1 t1 RIGHT JOIN table2 t2 ON t1.num_pk = t2.num_fk;
+
+#합집합 , 테이블A UNION 테이블B : 중복 제외한 2개이상 테이블 합치기 , ORACLE : FULL OUTER JOIN
+SELECT * FROM table1 t1 LEFT JOIN table2 t2 ON t1.num_pk = t2.num_fk UNION
+SELECT * FROM table1 t1 RIGHT JOIN tabel2 t2 ON t1.num_pk = t2.num_fk;
+
+#차집합 , 테이블A LEFT JOIN 테이블B ON 조인조건 WHERE 테이블B.FK IS NULL
+SELECT num_pk FROM table1 t1 LEFT JOIN table2 t2 ON t1.num_pk = t2.num_fk WHERE num_fk IS NULL;
+SELECT num_pk FROM table1 t1 RIGHT JOIN table2 t2 ON t1.num_pk = t2.num_fk WHERE num_fk IS NULL;
--- src/day0813/종합예제/view/BoardView.java
+++ src/day0813/종합예제/view/BoardView.java
@@ -10,45 +10,26 @@ public class BoardView {
     private BoardView(){} // 1.
     private static final BoardView instance = new BoardView(); // 2.
     public static BoardView getInstance( ){ return instance; } // 3. 
-
     // [*] MVC패턴 흐름의 controller 싱글톤 호출
     private BoardController bc = BoardController.getInstance();
-    // 입력객체
+    // [*] 입력객체
     private Scanner scan = new Scanner(System.in);
-    //최초 루프/화면
-    public void run(){
-        while(true){
-            try{
-            System.out.println("1.등록  2.전체조회  3.개별수정  4.개별삭제  선택: ");
-            int ch = scan.nextInt();
-            if (ch == 1){
-                save();
-            }
-            else if(ch == 2){}
-            else if(ch == 3){}
-            else if(ch == 4){}
-            }
-            catch (InputMismatchException e) {
-                scan = new Scanner(System.in); //입력받은 값의 타입이 일치하지 않을때 입력객체 초기화
-                System.out.println("다시 입력"+e);
+    // [*] 최초 루프/화면
+    public void run( ){
+        while( true ){
+            try{ // 예외처리 
+                System.out.print("1.등록 2.전체조회 3.개별수정 4.개별삭제 선택:");
+                int ch = scan.nextInt();
+                if( ch == 1 ){ }
+                else if( ch == 2 ){ }
+                else if( ch == 3 ){ }
+                else if( ch == 4 ){ }
+            }catch( InputMismatchException e ){
+                // 입력(성공) 했지만 타입반환에서 예외 이므로 입력객체 초기화
+                scan = new Scanner( System.in );
+                System.out.println("[다시입력]" + e);
             }
         }
     }
-
-    // 1. 등록 view
-    public void save(){
-        System.out.print("내용: ");
-        String 내용 = scan.next();
-        System.out.print("\n작성자: ");
-        String 작성자 = scan.next();
-        BoardDto boardDto = new BoardDto(0,내용,작성자);
-        boolean result = bc.save(boardDto;)
-        if(result){
-            System.out.println("등록성공!");
-        }
-        else{
-            System.out.println("등록실패!");
-        }
-    }
-}
+} // class end 
 
